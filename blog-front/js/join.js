796d03b96fd5e19941785c8eb8c068ff498fbfd3
갸갸 <quietwave00@gmail.com>
2023-04-24 20:38:38 +0900

    feat: 회원가입 프론트

---
+++ blog-front/js/join.js
@@ -0,0 +1,21 @@
+document.getElementById('join-button').addEventListener('click', () => {
+    fetch('http://localhost:8080/blog/join', {
+        method: 'POST',
+        headers: {
+            "content-type": "application/json",
+        },
+        body: JSON.stringify({
+            "userId": document.getElementById('userId').value,
+            "password": document.getElementById('password').value,
+            "nickname": document.getElementById('nickname').value
+        })
+    })
+    .then((res) => res.json())
+    .then(res => {
+        if(res.success == true) {
+            console.log("Join Success");
+            alert(`${res.response.nickname}님 가입을 축하합니다.`);
+            
+        }
+    })
+})
